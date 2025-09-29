010d8564725c427960c4026912c3e75430522702
xinhnh <xinhnh@seatechit.com.vn>
2025-09-29 20:09:21 +0700

    Initial Commit

---
+++ PhotoPiscumTests/PhotoPiscumTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPiscumTests.swift
+//  PhotoPiscumTests
+//
+//  Created by Dulcie on 9/29/25.
+//
+
+import Testing
+@testable import PhotoPiscum
+
+struct PhotoPiscumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
